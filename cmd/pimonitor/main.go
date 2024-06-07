e9ce773ad25a8d41584cbf719a78bfbc19c5ced8
Jesse Hernandez <futcomker@gmail.com>
2024-06-07 13:27:57 -0700

    Capture ds28b2 folder

--- cmd/pimonitor/main.go
+++ cmd/pimonitor/main.go
@@ -6,16 +6,27 @@ import (
 	"os"
 )
 
-func main() {
-	fmt.Printf("Attemping to capture temperature...")
-	  entries, err := os.ReadDir("/sys/bus/w1/devices")
-    if err != nil {
-        log.Fatal(err)
-    }
-
-		for _, e := range entries {
-			fmt.Println(e.Name)
+func VerifyPathExists(path string) (string, error) {
+    entries, err := os.ReadDir(path)
+		if err != nil {
+			log.Fatal(err)
 		}
 
-		fmt.Print(entries)
+		for _, e := range entries {
+			return e.Name(), nil
+ 		}
+	return "error", nil 
+}
+
+func main() {
+	ds18b20, err := VerifyPathExists("/sys/bus/w1/devices")
+	if err != nil {
+		log.Fatal("Could not capture")
+  }
+
+	fmt.Println(ds18b20)
+	//if err != nil { fmt.Print("Impossible, sensor is not configured correctly!") }
+
+
+	
 }
