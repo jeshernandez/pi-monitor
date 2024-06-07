5fd0c5ec6f4ffcb9304bce89482269332f3a4ed9
Jesse Hernandez <futcomker@gmail.com>
2024-06-07 13:34:34 -0700

    Try reading file

--- cmd/pimonitor/main.go
+++ cmd/pimonitor/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 )
 
 func VerifyPathExists(path string) (string, error) {
@@ -19,14 +21,26 @@ func VerifyPathExists(path string) (string, error) {
 }
 
 func main() {
-	ds18b20, err := VerifyPathExists("/sys/bus/w1/devices")
+	path := "/sys/bus/w1/devices"
+	ds18b20, err := VerifyPathExists(path)
+	location := path + ds18b20 + "/w1_slave"
+
+	fileIO, err := os.OpenFile(location, os.O_RDWR, 0600)
 	if err != nil {
-		log.Fatal("Could not capture")
-  }
+		panic(err)
+	}
 
-	fmt.Println(ds18b20)
-	//if err != nil { fmt.Print("Impossible, sensor is not configured correctly!") }
-
+	defer fileIO.Close()
+	rawBytes, err := ioutil.ReadAll(fileIO)
+	if err != nil {
+		panic(err)
+	}
+	lines := strings.Split(string(rawBytes), "\n")
+	for i, line := range lines {
+		if i == 2 {
+			fmt.Println(line)
+		}
+	}
 
 	
 }
